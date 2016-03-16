8d66dc9e76964491ee4ca5acae06b6400b856290
Thomas Reis <thomas.reis@cern.ch>
2016-03-16 17:50:29 +0100

    uGMT ntuples and config files

--- L1Trigger/L1TNtuples/interface/L1AnalysisGeneratorDataFormat.h
+++ L1Trigger/L1TNtuples/interface/L1AnalysisGeneratorDataFormat.h
@@ -3,8 +3,8 @@
 
 //-------------------------------------------------------------------------------
 // Created 15/04/2010 - E. Conte, A.C. Le Bihan
-// 
-// 
+//
+//
 // Original code : L1Trigger/L1TNtuples/L1NtupleProducer
 //-------------------------------------------------------------------------------
 #include <TROOT.h>
@@ -16,59 +16,45 @@ namespace L1Analysis
 {
   struct L1AnalysisGeneratorDataFormat
   {
-  
+
     L1AnalysisGeneratorDataFormat(){Reset();};
     ~L1AnalysisGeneratorDataFormat(){};
-    
+
     void Reset()
     {
      weight = -999.;
      pthat  = -999.;
-     nVtx   = 0;
-     nPUPoissonMean = 0;
-
-     nPart = 0;
-     partId.resize(0);
-     partStat.resize(0);
-     partParent.resize(0);
-     partPt.resize(0);
-     partEta.resize(0);
-     partPhi.resize(0);
-     partE.resize(0);
-
-     nJet = 0;     
-     jetPt.resize(0);
-     jetEta.resize(0);
-     jetPhi.resize(0);
-     jetM.resize(0);
+                id.resize(0);
+		status.resize(0);
+		parent_id.resize(0);
+		px.resize(0);
+		py.resize(0);
+		pz.resize(0);
+		e.resize(0);
+    pt.resize(0);
+    phi.resize(0);
+    eta.resize(0);
 
     }
 
-                   
+
     // ---- L1AnalysisGeneratorDataFormat information.
-    
+
     float weight;
     float pthat;
-    int nVtx;
-    int nPUPoissonMean;
+    std::vector<int> id;
+    std::vector<int> status;
+    std::vector<int> parent_id;
+    std::vector<float> px;
+    std::vector<float> py;
+    std::vector<float> pz;
+    std::vector<float> e;
+    std::vector<float> pt;
+    std::vector<float> phi;
+    std::vector<float> eta;
 
-    int nPart;
-    std::vector<int> partId;
-    std::vector<int> partStat;
-    std::vector<int> partParent;
-    std::vector<float> partPt;
-    std::vector<float> partEta;
-    std::vector<float> partPhi;
-    std::vector<float> partE;
-    
-    int nJet;
-    std::vector<float> jetPt;
-    std::vector<float> jetEta;
-    std::vector<float> jetPhi;
-    std::vector<float> jetM;
-
-  }; 
-} 
+  };
+}
 #endif
 
 
