c3cb35ccd2db516d37b7f77720a9c2634c69e4eb
Thomas Reis <thomas.reis@cern.ch>
2016-04-25 13:26:59 +0200

    Disable cfg from DB until tested

--- L1Trigger/L1TMuon/python/fakeGmtParams_cff.py
+++ L1Trigger/L1TMuon/python/fakeGmtParams_cff.py
@@ -21,7 +21,7 @@ gmtParams = cms.ESProducer('L1TMuonGlobalParamsESProducer',
     fwVersion = cms.uint32(1),
 
     # get configuration from DB and ignore values in this file
-    configFromDb = cms.bool(True),
+    configFromDb = cms.bool(False),
 
     # uGMT inputs to disable
     # disabled inputs are not used in the algo but are still in the readout
