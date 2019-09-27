f0e40f6fe337bcfcb210aa3d9fa2323f4d47834c
Thomas Reis <thomas.reis@cern.ch>
2019-09-27 12:35:59 +0200

    Add L1PhaseIIEGTkIsoTreeProducer to L1 ntuples.

---
+++ L1Trigger/L1TNtuples/plugins/L1PhaseIIEGTkIsoTreeProducer.cc
@@ -0,0 +1,185 @@
+// -*- C++ -*-
+//
+// Package:    L1Trigger/L1TNtuples
+// Class:      L1PhaseIIEGTkIsoTreeProducer
+//
+/**\class L1PhaseIIEGTkIsoTreeProducer L1PhaseIIEGTkIsoTreeProducer.cc L1Trigger/L1TNtuples/plugins/L1PhaseIIEGTkIsoTreeProducer.cc
+
+ Description: Produces an L1 ntuple branch for phase II EG tracker isolation studies
+
+ Implementation:
+     [Notes on implementation]
+*/
+//
+// Original Author:  Thomas Reis
+//         Created:  Wed, 11 Sep 2019 14:55:42 GMT
+//
+//
+
+
+// system include files
+#include <memory>
+#include <string>
+
+// user include files
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/one/EDAnalyzer.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/Utilities/interface/InputTag.h"
+
+// data formats
+#include "DataFormats/L1Trigger/interface/EGamma.h"
+#include "DataFormats/L1TrackTrigger/interface/TTTypes.h"
+
+#include "Geometry/Records/interface/TrackerDigiGeometryRecord.h"
+#include "Geometry/TrackerGeometryBuilder/interface/TrackerGeometry.h"
+
+// ROOT output stuff
+#include "FWCore/ServiceRegistry/interface/Service.h"
+#include "CommonTools/UtilAlgos/interface/TFileService.h"
+#include "TTree.h"
+
+#include "L1Trigger/L1TNtuples/interface/L1AnalysisPhaseIIEGTkIso.h"
+
+//
+// class declaration
+//
+class L1PhaseIIEGTkIsoTreeProducer : public edm::one::EDAnalyzer<edm::one::SharedResources>  {
+  public:
+    explicit L1PhaseIIEGTkIsoTreeProducer(const edm::ParameterSet&);
+    ~L1PhaseIIEGTkIsoTreeProducer();
+
+    static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
+
+    std::shared_ptr<L1Analysis::L1AnalysisPhaseIIEGTkIso> l1Phase2EGTkIso_;
+    L1Analysis::L1AnalysisPhaseIIEGTkIsoDataFormat *l1Phase2EGTkIsoData_;
+
+    typedef TTTrack<Ref_Phase2TrackerDigi_> L1TTTrackType;
+    typedef std::vector<L1TTTrackType> L1TTTrackCollectionType;
+
+  private:
+    virtual void beginJob() override;
+    virtual void analyze(const edm::Event&, const edm::EventSetup&) override;
+    virtual void endJob() override;
+
+    // ----------member data ---------------------------
+
+    // output file
+    edm::Service<TFileService> fs_;
+
+    // tree
+    TTree* tree_;
+
+    edm::EDGetTokenT<l1t::EGammaBxCollection> egToken_;
+    edm::EDGetTokenT<l1t::EGammaBxCollection> egTokenHGC_;
+    edm::EDGetTokenT<L1TTTrackCollectionType> tttrackToken_;
+
+    std::string trackerGeom_;
+};
+
+//
+// constructors and destructor
+//
+L1PhaseIIEGTkIsoTreeProducer::L1PhaseIIEGTkIsoTreeProducer(const edm::ParameterSet& iConfig) :
+  l1Phase2EGTkIso_(std::make_shared<L1Analysis::L1AnalysisPhaseIIEGTkIso>(iConfig)),
+  tree_(fs_->make<TTree>("L1PhaseIIEGTkIsoTree", "L1PhaseIIEGTkIsoTree")),
+  egToken_(consumes<l1t::EGammaBxCollection>(iConfig.getParameter<edm::InputTag>("l1EgBarrel"))),
+  egTokenHGC_(consumes<l1t::EGammaBxCollection>(iConfig.getParameter<edm::InputTag>("l1EgHGC"))),
+  tttrackToken_(consumes<L1TTTrackCollectionType>(iConfig.getParameter<edm::InputTag>("l1Tracks"))),
+  trackerGeom_(iConfig.getParameter<std::string>("trackerGeometry"))
+{
+  l1Phase2EGTkIsoData_ = l1Phase2EGTkIso_->GetData();
+
+  tree_->Branch("L1PhaseIIEGTkIso", "L1Analysis::L1AnalysisPhaseIIEGTkIsoDataFormat", &l1Phase2EGTkIsoData_, 32000, 3);
+}
+
+
+L1PhaseIIEGTkIsoTreeProducer::~L1PhaseIIEGTkIsoTreeProducer()
+{
+}
+
+
+//
+// member functions
+//
+
+// ------------ method called for each event  ------------
+void
+L1PhaseIIEGTkIsoTreeProducer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
+{
+  // first reset the branch
+  l1Phase2EGTkIso_->Reset();
+
+  // get the collections
+  edm::Handle<l1t::EGammaBxCollection> egBarrel;
+  iEvent.getByToken(egToken_, egBarrel);
+  if (!egBarrel.isValid()) {
+    edm::LogWarning("MissingProduct") << "Barrel Egamma BX collection not found in the event. Branch will not be filled";
+    return;
+  }
+
+  edm::Handle<l1t::EGammaBxCollection> egHGC;
+  iEvent.getByToken(egTokenHGC_, egHGC);
+  if (!egHGC.isValid()) {
+    edm::LogWarning("MissingProduct") << "HGC Egamma BX collection not found in the event. Branch will not be filled";
+    return;
+  }
+
+  edm::Handle<L1TTTrackCollectionType> tttrack;
+  iEvent.getByToken(tttrackToken_, tttrack);
+  if (!tttrack.isValid()) {
+    edm::LogWarning("MissingProduct") << "TT track collection not found in the event. Branch will not be filled";
+    return;
+  }
+
+  // geometry needed to call pTFrom2Stubs
+  edm::ESHandle<TrackerGeometry> geomHandle;
+  iSetup.get<TrackerDigiGeometryRecord>().get(trackerGeom_, geomHandle);
+  const auto tGeom = geomHandle.product();
+
+  // set the branches
+  l1Phase2EGTkIso_->SetEGWithTracks(egBarrel, egHGC, tttrack, tGeom);
+
+  //fill the tree
+  tree_->Fill();
+}
+
+
+// ------------ method called once each job just before starting event loop  ------------
+void
+L1PhaseIIEGTkIsoTreeProducer::beginJob()
+{
+}
+
+// ------------ method called once each job just after ending the event loop  ------------
+void
+L1PhaseIIEGTkIsoTreeProducer::endJob()
+{
+}
+
+// ------------ method fills 'descriptions' with the allowed parameters for the module  ------------
+void
+L1PhaseIIEGTkIsoTreeProducer::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
+  edm::ParameterSetDescription desc;
+  desc.add<edm::InputTag>("l1EgBarrel");
+  desc.add<edm::InputTag>("l1EgHGC");
+  desc.add<edm::InputTag>("l1Tracks");
+  desc.add<std::string>("trackerGeometry", "idealForDigi");
+  desc.add<double>("egBarrelMinEt", 5.);
+  desc.add<double>("egHGCMinEt", 10.);
+  desc.add<double>("trackMinPt", 0.);
+  desc.add<double>("trackMaxChi2", 1e10);
+  desc.add<bool>("useTwoStubsPt", false);
+  desc.add<std::vector<double>>("trackEGammaDeltaPhi", {0.07, 0., 0.});
+  desc.add<std::vector<double>>("trackEGammaDeltaR", {0.08, 0., 0.});
+  desc.add<double>("trackMinPtForIso", 2.);
+  desc.add<double>("trackMaxChi2ForIso", 1e10);
+  desc.add<double>("dRMinForIso", 0.);
+  desc.add<double>("dRMaxForIso", 1.);
+  descriptions.addDefault(desc);
+}
+
+//define this as a plug-in
+DEFINE_FWK_MODULE(L1PhaseIIEGTkIsoTreeProducer);
