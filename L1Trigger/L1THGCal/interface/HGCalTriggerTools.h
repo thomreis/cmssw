e491a7f7cf251adf4ad89d75b1181c81055c2591
Gianluca <gianluca.cerminara@cern.ch>
2019-10-01 10:21:59 +0200

    add framework for energy interpretation and implement EM one

--- L1Trigger/L1THGCal/interface/HGCalTriggerTools.h
+++ L1Trigger/L1THGCal/interface/HGCalTriggerTools.h
@@ -81,6 +81,7 @@ public:
 
   DetId simToReco(const DetId&, const HGCalTopology&) const;
   DetId simToReco(const DetId&, const HcalTopology&) const;
+  unsigned triggerLayer(const unsigned id) const { return geom_->triggerLayer(id); }
 
 private:
   const HGCalTriggerGeometryBase* geom_;
