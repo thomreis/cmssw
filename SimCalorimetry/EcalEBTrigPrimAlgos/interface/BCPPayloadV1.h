58d02ad936b038a1306b2723c7fa414a9a0ad3e1
Thomas Reis <thomas.reis@cern.ch>
2020-02-28 16:06:17 +0000

    Add TP clustering algo with swiss cross spike flag.

--- SimCalorimetry/EcalEBTrigPrimAlgos/interface/BCPPayloadV1.h
+++ SimCalorimetry/EcalEBTrigPrimAlgos/interface/BCPPayloadV1.h
@@ -3,6 +3,7 @@
 
 #include "SimCalorimetry/EcalEBTrigPrimAlgos/interface/BCPPayload.h"
 #include "SimCalorimetry/EcalEBTrigPrimAlgos/interface/SpikeTaggerLDAlgo.h"
+#include "SimCalorimetry/EcalEBTrigPrimAlgos/interface/TPClusterAlgo.h"
 
 namespace ecalPh2 {
 
@@ -11,12 +12,13 @@ class BCPPayloadV1 : public BCPPayload {
   BCPPayloadV1(const std::shared_ptr<ecalPh2::EcalBcpPayloadParamsHelper> ecalBcpPayloadParamsHelper, const edm::EventSetup &eventSetup) : BCPPayload(ecalBcpPayloadParamsHelper, eventSetup) { createAlgos(eventSetup); };
   virtual ~BCPPayloadV1() {};
 
-  void processEvent(const EBDigiCollection &ebDigis, EcalEBTrigPrimDigiCollection &ebTPs) override;
+  void processEvent(const EBDigiCollection &ebDigis, EcalEBTrigPrimDigiCollection &ebTPs, std::vector<EcalEBTriggerPrimitiveCluster> &ebTPClusters) override;
 
  protected:
   void createAlgos(const edm::EventSetup &eventSetup) override;
 
   std::unique_ptr<SpikeTaggerLDAlgo> spikeTaggerLDAlgo_;
+  std::unique_ptr<TPClusterAlgo> tpClusterAlgo_;
 };
 
 } // namespace ecalPh2
