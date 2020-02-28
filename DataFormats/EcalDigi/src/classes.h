b29a86109c4ab6b7e54354e92dd421da4f99848c
Thomas Reis <thomas.reis@cern.ch>
2020-02-28 10:05:30 +0000

    Add EcalEBTriggerPrimitiveCluster data format.

--- DataFormats/EcalDigi/src/classes.h
+++ DataFormats/EcalDigi/src/classes.h
@@ -4,3 +4,4 @@
 #include "DataFormats/EcalDigi/interface/EcalDigiSoA.h"
 #include "DataFormats/EcalDigi/interface/EcalDigiPhase2HostCollection.h"
 #include "DataFormats/EcalDigi/interface/EcalDigiPhase2SoA.h"
+#include "DataFormats/EcalDigi/interface/EcalEBTriggerPrimitiveCluster.h"
