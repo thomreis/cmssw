f25bb54a3a3329c4975bd85c4ce81d394abe07b3
Thomas Reis <thomas.reis@cern.ch>
2020-02-28 16:06:17 +0000

    Add TP clustering algo with swiss cross spike flag.

--- SimCalorimetry/EcalEBTrigPrimAlgos/src/BCPPayloadV1.cc
+++ SimCalorimetry/EcalEBTrigPrimAlgos/src/BCPPayloadV1.cc
@@ -13,17 +13,21 @@
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
 #include "SimCalorimetry/EcalEBTrigPrimAlgos/interface/BCPPayloadV1.h"
 #include "SimCalorimetry/EcalEBTrigPrimAlgos/interface/SpikeTaggerLDAlgoFactory.h"
+#include "SimCalorimetry/EcalEBTrigPrimAlgos/interface/TPClusterAlgoFactory.h"
 
-void ecalPh2::BCPPayloadV1::processEvent(const EBDigiCollection &ebDigis, EcalEBTrigPrimDigiCollection &ebTPs)
+void ecalPh2::BCPPayloadV1::processEvent(const EBDigiCollection &ebDigis, EcalEBTrigPrimDigiCollection &ebTPs, std::vector<EcalEBTriggerPrimitiveCluster> &ebTPClusters)
 {
   std::cout << "Processing BCPPayloadV1" << std::endl;
   spikeTaggerLDAlgo_->processEvent(ebDigis, ebTPs);
+  tpClusterAlgo_->processEvent(ebTPs, ebTPClusters);
 }
 
 void ecalPh2::BCPPayloadV1::createAlgos(const edm::EventSetup &eventSetup)
 {
-  ecalPh2::SpikeTaggerLDAlgoFactory spikeTaggerLDFactory;
+  ecalPh2::SpikeTaggerLDAlgoFactory spikeTaggerLDAlgoFactory;
+  ecalPh2::TPClusterAlgoFactory tpClusterAlgoFactory;
 
-  spikeTaggerLDAlgo_ = spikeTaggerLDFactory.create(ecalBcpPayloadParamsHelper_, eventSetup);
+  spikeTaggerLDAlgo_ = spikeTaggerLDAlgoFactory.create(ecalBcpPayloadParamsHelper_, eventSetup);
+  tpClusterAlgo_ = tpClusterAlgoFactory.create(ecalBcpPayloadParamsHelper_);
 }
 
