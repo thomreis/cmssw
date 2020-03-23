fd12d95f477b4d1a3ec0ee281cbc711d50661ad1
Thomas Reis <thomas.reis@cern.ch>
2020-03-23 15:49:22 +0000

    Fix EcalEBTriggerPrimitiveCluster data format.

--- DataFormats/EcalDigi/interface/EcalEBTriggerPrimitiveCluster.h
+++ DataFormats/EcalDigi/interface/EcalEBTriggerPrimitiveCluster.h
@@ -1,25 +1,27 @@
 #ifndef DataFormats_EcalDigi_EcalEBTriggerPrimitiveCluster_h
 #define DataFormats_EcalDigi_EcalEBTriggerPrimitiveCluster_h
 
+#include <vector>
+
 class EcalEBTriggerPrimitiveCluster {
  public:
   EcalEBTriggerPrimitiveCluster() : hwEt_(0), hwTime_(0), hwEta_(0), hwPhi_(0), numberOfCrystals_(0), spike_(false) {}
   EcalEBTriggerPrimitiveCluster(int hwEt, int hwTime, int hwEta, int hwPhi, int numberOfCrystals, bool spike) : hwEt_(hwEt), hwTime_(hwTime), hwEta_(hwEta), hwPhi_(hwPhi), numberOfCrystals_(numberOfCrystals), spike_(spike) {}
 
   void setHwEt(const int hwEt) { hwEt_ = hwEt; }
-  bool hwEt() const { return hwEt_; }
+  int hwEt() const { return hwEt_; }
 
   void setHwTime(const int hwTime) { hwTime_ = hwTime; }
-  bool hwTime() const { return hwTime_; }
+  int hwTime() const { return hwTime_; }
 
   void setHwEta(const int hwEta) { hwEta_ = hwEta; }
-  bool hwEta() const { return hwEta_; }
+  int hwEta() const { return hwEta_; }
 
   void setHwPhi(const int hwPhi) { hwPhi_ = hwPhi; }
-  bool hwPhi() const { return hwPhi_; }
+  int hwPhi() const { return hwPhi_; }
 
   void setNumberOfCrystals(const int numberOfCrystals) { numberOfCrystals_ = numberOfCrystals; }
-  bool numberOfCrystals() const { return numberOfCrystals_; }
+  int numberOfCrystals() const { return numberOfCrystals_; }
 
   void setSpike(const bool spike) { spike_ = spike; }
   bool spike() const { return spike_; }
@@ -37,4 +39,5 @@ class EcalEBTriggerPrimitiveCluster {
   bool spike_;
 };
 
+typedef std::vector<EcalEBTriggerPrimitiveCluster> EcalEBTriggerPrimitiveClusterCollection;
 #endif
