2e97c7a20cfeb2084d0dbe93ee2157c1136897bc
Vladimir <vladimir.rekovic@cern.ch>
2020-03-30 11:26:49 +0200

    DataFormats/Phase2L1Correlator rename files removing pattern Partcle from name.

--- DataFormats/Phase2L1Correlator/src/classes.h
+++ DataFormats/Phase2L1Correlator/src/classes.h
@@ -15,40 +15,40 @@
 
 // includes needed for the L1TrackTriggerObjects
 
-#include "DataFormats/Phase2L1Correlator/interface/L1TkPrimaryVertex.h"
-#include "DataFormats/Phase2L1Correlator/interface/L1TkEtMissParticle.h"
-#include "DataFormats/Phase2L1Correlator/interface/L1TkEtMissParticleFwd.h"
+#include "DataFormats/Phase2L1Correlator/interface/TkPrimaryVertex.h"
+#include "DataFormats/Phase2L1Correlator/interface/TkEtMiss.h"
+#include "DataFormats/Phase2L1Correlator/interface/TkEtMissFwd.h"
 
-#include "DataFormats/Phase2L1Correlator/interface/L1TkEmParticle.h"
-#include "DataFormats/Phase2L1Correlator/interface/L1TkEmParticleFwd.h"
+#include "DataFormats/Phase2L1Correlator/interface/TkEm.h"
+#include "DataFormats/Phase2L1Correlator/interface/TkEmFwd.h"
 
-#include "DataFormats/Phase2L1Correlator/interface/L1TkEGTauParticle.h"
-#include "DataFormats/Phase2L1Correlator/interface/L1TrkTauParticle.h"
-#include "DataFormats/Phase2L1Correlator/interface/L1CaloTkTauParticle.h"
+#include "DataFormats/Phase2L1Correlator/interface/TkEGTau.h"
+#include "DataFormats/Phase2L1Correlator/interface/L1TrkTau.h"
+#include "DataFormats/Phase2L1Correlator/interface/L1CaloTkTau.h"
 
-#include "DataFormats/Phase2L1Correlator/interface/L1TkElectronParticle.h"
-#include "DataFormats/Phase2L1Correlator/interface/L1TkElectronParticleFwd.h"
+#include "DataFormats/Phase2L1Correlator/interface/TkElectron.h"
+#include "DataFormats/Phase2L1Correlator/interface/TkElectronFwd.h"
 
-#include "DataFormats/Phase2L1Correlator/interface/L1TkJetParticle.h"
-#include "DataFormats/Phase2L1Correlator/interface/L1TkJetParticleFwd.h"
+#include "DataFormats/Phase2L1Correlator/interface/TkJet.h"
+#include "DataFormats/Phase2L1Correlator/interface/TkJetFwd.h"
 
-#include "DataFormats/Phase2L1Correlator/interface/L1TkHTMissParticle.h"
-#include "DataFormats/Phase2L1Correlator/interface/L1TkHTMissParticleFwd.h"
+#include "DataFormats/Phase2L1Correlator/interface/TkHTMiss.h"
+#include "DataFormats/Phase2L1Correlator/interface/TkHTMissFwd.h"
 
-#include "DataFormats/Phase2L1Correlator/interface/L1TkMuonParticle.h"
-#include "DataFormats/Phase2L1Correlator/interface/L1TkMuonParticleFwd.h"
+#include "DataFormats/Phase2L1Correlator/interface/TkMuon.h"
+#include "DataFormats/Phase2L1Correlator/interface/TkMuonFwd.h"
 
-#include "DataFormats/Phase2L1Correlator/interface/L1TkGlbMuonParticle.h"
-#include "DataFormats/Phase2L1Correlator/interface/L1TkGlbMuonParticleFwd.h"
+#include "DataFormats/Phase2L1Correlator/interface/TkGlbMuon.h"
+#include "DataFormats/Phase2L1Correlator/interface/TkGlbMuonFwd.h"
 
-#include "DataFormats/Phase2L1Correlator/interface/L1TkTauParticle.h"
-#include "DataFormats/Phase2L1Correlator/interface/L1TkTauParticleFwd.h"
+#include "DataFormats/Phase2L1Correlator/interface/TkTau.h"
+#include "DataFormats/Phase2L1Correlator/interface/TkTauFwd.h"
 
-#include "DataFormats/Phase2L1Correlator/interface/L1TkPhiCandidate.h"
-#include "DataFormats/Phase2L1Correlator/interface/L1TkPhiCandidateFwd.h"
+#include "DataFormats/Phase2L1Correlator/interface/TkPhiCandidate.h"
+#include "DataFormats/Phase2L1Correlator/interface/TkPhiCandidateFwd.h"
 
-#include "DataFormats/Phase2L1Correlator/interface/L1TkBsCandidate.h"
-#include "DataFormats/Phase2L1Correlator/interface/L1TkBsCandidateFwd.h"
+#include "DataFormats/Phase2L1Correlator/interface/TkBsCandidate.h"
+#include "DataFormats/Phase2L1Correlator/interface/TkBsCandidateFwd.h"
 
 #include "DataFormats/Common/interface/RefVector.h"
 #include "DataFormats/Common/interface/RefProd.h"
@@ -60,88 +60,88 @@ namespace {
   struct dictionaryl1tkobj {
 
     // L1 Primary Vertex
-    l1t::L1TkPrimaryVertex trzvtx;
-    edm::Wrapper<l1t::L1TkPrimaryVertexCollection> trzvtxColl;
-    edm::Ref<l1t::L1TkPrimaryVertexCollection> trkvtxRef;
+    l1t::TkPrimaryVertex trzvtx;
+    edm::Wrapper<l1t::TkPrimaryVertexCollection> trzvtxColl;
+    edm::Ref<l1t::TkPrimaryVertexCollection> trkvtxRef;
 
-    // L1TkEtMiss... following L1EtMiss...
-    l1t::L1TkEtMissParticle TketMiss;
-    l1t::L1TkEtMissParticleCollection TketMissColl;
-    edm::Wrapper<l1t::L1TkEtMissParticle> w_TketMiss;
-    edm::Wrapper<l1t::L1TkEtMissParticleCollection> w_TketMissColl;
-    //l1t::L1TkEtMissParticleRef refTkEtMiss ;
-    //l1t::L1TkEtMissParticleRefVector refTkVecEtMiss ;
-    //l1t::L1TkEtMissParticleVectorRef vecTkRefEtMissColl ;
-    //l1t::L1TkEtMissParticleRefProd refTkProdEtMiss ;
-    //edm::reftobase::Holder<reco::Candidate, l1t::L1TkEtMissParticleRef> rtbTkm1;
-    //edm::reftobase::Holder<reco::Candidate, l1t::L1TkEtMissParticleRefProd> rtbTkm2;
+    // TkEtMiss... following L1EtMiss...
+    l1t::TkEtMiss TketMiss;
+    l1t::TkEtMissCollection TketMissColl;
+    edm::Wrapper<l1t::TkEtMiss> w_TketMiss;
+    edm::Wrapper<l1t::TkEtMissCollection> w_TketMissColl;
+    //l1t::TkEtMissRef refTkEtMiss ;
+    //l1t::TkEtMissRefVector refTkVecEtMiss ;
+    //l1t::TkEtMissVectorRef vecTkRefEtMissColl ;
+    //l1t::TkEtMissRefProd refTkProdEtMiss ;
+    //edm::reftobase::Holder<reco::Candidate, l1t::TkEtMissRef> rtbTkm1;
+    //edm::reftobase::Holder<reco::Candidate, l1t::TkEtMissRefProd> rtbTkm2;
 
-    // L1TkEmParticle
-    l1t::L1TkEmParticleCollection trkemColl;
-    edm::Wrapper<l1t::L1TkEmParticleCollection> w_trkemColl;
-    l1t::L1TkEmParticleRef reftrkEm;
-    //l1t::L1TkEmParticleRefVector refVectrkEmColl ;
-    //l1t::L1TkEmParticleVectorRef vecReftrkEmColl ;
-    //edm::reftobase::Holder<reco::Candidate, l1t::L1TkEmParticleRef> rtbtrke;
+    // TkEm
+    l1t::TkEmCollection trkemColl;
+    edm::Wrapper<l1t::TkEmCollection> w_trkemColl;
+    l1t::TkEmRef reftrkEm;
+    //l1t::TkEmRefVector refVectrkEmColl ;
+    //l1t::TkEmVectorRef vecReftrkEmColl ;
+    //edm::reftobase::Holder<reco::Candidate, l1t::TkEmRef> rtbtrke;
 
-    // L1TkEGTauParticle
-    l1t::L1TkEGTauParticleCollection trkegColl;
-    edm::Wrapper<l1t::L1TkEGTauParticleCollection> w_trkegColl;
-    l1t::L1TkEGTauParticleRef reftrkEG;
+    // TkEGTau
+    l1t::TkEGTauCollection trkegColl;
+    edm::Wrapper<l1t::TkEGTauCollection> w_trkegColl;
+    l1t::TkEGTauRef reftrkEG;
 
-    // L1TrkTauParticle
-    l1t::L1TrkTauParticleCollection tktauColl;
-    edm::Wrapper<l1t::L1TrkTauParticleCollection> w_tktauColl;
-    l1t::L1TrkTauParticleRef reftkTau;
+    // L1TrkTau
+    l1t::L1TrkTauCollection tktauColl;
+    edm::Wrapper<l1t::L1TrkTauCollection> w_tktauColl;
+    l1t::L1TrkTauRef reftkTau;
 
-    // L1CaloTkTauParticle
-    l1t::L1CaloTkTauParticleCollection calotrktauColl;
-    edm::Wrapper<l1t::L1CaloTkTauParticleCollection> w_calotrktauColl;
-    l1t::L1CaloTkTauParticleRef refcalotrkTau;
+    // L1CaloTkTau
+    l1t::L1CaloTkTauCollection calotrktauColl;
+    edm::Wrapper<l1t::L1CaloTkTauCollection> w_calotrktauColl;
+    l1t::L1CaloTkTauRef refcalotrkTau;
 
-    // L1TkElectronParticle
-    l1t::L1TkElectronParticleCollection trkeleColl;
-    edm::Wrapper<l1t::L1TkElectronParticleCollection> w_trkeleColl;
-    l1t::L1TkElectronParticleRef reftrkEle;
+    // TkElectron
+    l1t::TkElectronCollection trkeleColl;
+    edm::Wrapper<l1t::TkElectronCollection> w_trkeleColl;
+    l1t::TkElectronRef reftrkEle;
 
-    // L1TkJetParticle
-    l1t::L1TkJetParticleCollection trkjetColl;
-    edm::Wrapper<l1t::L1TkJetParticleCollection> w_trkjetColl;
-    l1t::L1TkJetParticleRef reftrkJet;
-    l1t::L1TkJetParticleRefProd refTkProdJet;
+    // TkJet
+    l1t::TkJetCollection trkjetColl;
+    edm::Wrapper<l1t::TkJetCollection> w_trkjetColl;
+    l1t::TkJetRef reftrkJet;
+    l1t::TkJetRefProd refTkProdJet;
 
-    // L1TkHTMissParticle
-    l1t::L1TkHTMissParticle TkHTMiss;
-    l1t::L1TkHTMissParticleCollection TkHTMissColl;
-    edm::Wrapper<l1t::L1TkHTMissParticle> w_TkHTMiss;
-    edm::Wrapper<l1t::L1TkHTMissParticleCollection> w_TkHTMissColl;
+    // TkHTMiss
+    l1t::TkHTMiss TkHTMiss;
+    l1t::TkHTMissCollection TkHTMissColl;
+    edm::Wrapper<l1t::TkHTMiss> w_TkHTMiss;
+    edm::Wrapper<l1t::TkHTMissCollection> w_TkHTMissColl;
 
-    // L1TkMuonParticle
-    l1t::L1TkMuonParticleCollection trkmuColl ;
-    edm::Wrapper<l1t::L1TkMuonParticleCollection> w_trkmuColl;
-    l1t::L1TkMuonParticleRef reftrkMu ;
+    // TkMuon
+    l1t::TkMuonCollection trkmuColl ;
+    edm::Wrapper<l1t::TkMuonCollection> w_trkmuColl;
+    l1t::TkMuonRef reftrkMu ;
 
-    // L1TkGlbMuonParticle
-    l1t::L1TkGlbMuonParticleCollection trkglbmuColl;
-    edm::Wrapper<l1t::L1TkGlbMuonParticleCollection> w_trkglbmuColl;
-    l1t::L1TkGlbMuonParticleRef reftrkGlbMu;
+    // TkGlbMuon
+    l1t::TkGlbMuonCollection trkglbmuColl;
+    edm::Wrapper<l1t::TkGlbMuonCollection> w_trkglbmuColl;
+    l1t::TkGlbMuonRef reftrkGlbMu;
 
-    // L1TkTauParticle
-    l1t::L1TkTauParticleCollection trktauColl;
-    edm::Wrapper<l1t::L1TkTauParticleCollection> w_trktauColl;
-    l1t::L1TkTauParticleRef reftrkTau;
+    // TkTau
+    l1t::TkTauCollection trktauColl;
+    edm::Wrapper<l1t::TkTauCollection> w_trktauColl;
+    l1t::TkTauRef reftrkTau;
 
-    // L1TkPhiCandidate
-    l1t::L1TkPhiCandidateCollection trkPhiColl;
-    edm::Wrapper<l1t::L1TkPhiCandidateCollection> w_trkPhiColl;
-    l1t::L1TkPhiCandidateRef reftrkPhi;
-    //l1t::L1TkPhiCandidateRefProd refTkProdPhi;
+    // TkPhiCandidate
+    l1t::TkPhiCandidateCollection trkPhiColl;
+    edm::Wrapper<l1t::TkPhiCandidateCollection> w_trkPhiColl;
+    l1t::TkPhiCandidateRef reftrkPhi;
+    //l1t::TkPhiCandidateRefProd refTkProdPhi;
 
-    // L1TkBsCandidate
-    l1t::L1TkBsCandidateCollection trkBsColl;
-    edm::Wrapper<l1t::L1TkBsCandidateCollection> w_trkBsColl;
-    l1t::L1TkBsCandidateRef reftrkBs;
-    //l1t::L1TkBsCandidateRefProd refTkProdBs;
+    // TkBsCandidate
+    l1t::TkBsCandidateCollection trkBsColl;
+    edm::Wrapper<l1t::TkBsCandidateCollection> w_trkBsColl;
+    l1t::TkBsCandidateRef reftrkBs;
+    //l1t::TkBsCandidateRefProd refTkProdBs;
   };
 }  // namespace
 
