4701c04c043bbcc0f5c563af37d3a608f74de214
Nazar Bartosik <nazar.bartosik@cern.ch>
2021-08-25 18:42:51 +0200

    Added missing Phase2 classes for ECAL pulses

--- CondFormats/EcalObjects/interface/EcalPulseCovariances.h
+++ CondFormats/EcalObjects/interface/EcalPulseCovariances.h
@@ -8,9 +8,11 @@
 
 struct EcalPulseCovariance {
 public:
+  static const int TEMPLATESAMPLES = EcalPulseShape::TEMPLATESAMPLES;
+
   EcalPulseCovariance();
 
-  float covval[EcalPulseShape::TEMPLATESAMPLES][EcalPulseShape::TEMPLATESAMPLES];
+  float covval[TEMPLATESAMPLES][TEMPLATESAMPLES];
 
   float val(int i, int j) const { return covval[i][j]; }
 
@@ -20,5 +22,15 @@ public:
 typedef EcalCondObjectContainer<EcalPulseCovariance> EcalPulseCovariancesMap;
 typedef EcalPulseCovariancesMap::const_iterator EcalPulseCovariancesMapIterator;
 typedef EcalPulseCovariancesMap EcalPulseCovariances;
+
+
+struct EcalPh2PulseCovariance: public EcalPulseCovariance {
+public:
+  static const int TEMPLATESAMPLES = EcalPh2PulseShape::TEMPLATESAMPLES;
+};
+
+typedef EcalCondObjectContainer<EcalPh2PulseCovariance> EcalPh2PulseCovariancesMap;
+typedef EcalPh2PulseCovariancesMap::const_iterator EcalPh2PulseCovariancesMapIterator;
+typedef EcalPh2PulseCovariancesMap EcalPh2PulseCovariances;
 
 #endif
