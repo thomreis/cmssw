03c93927fb27439673c9cd2ac8dda8f9781b7f3a
Thomas Reis <thomas.reis@cern.ch>
2021-09-21 13:55:20 +0200

    Make L1EGPuppiIsoAlgo class independent of EDM data formats.

--- L1Trigger/Phase2L1ParticleFlow/plugins/L1TEGPuppiIsoProducer.cc
+++ L1Trigger/Phase2L1ParticleFlow/plugins/L1TEGPuppiIsoProducer.cc
@@ -3,12 +3,13 @@
 #include <vector>
 
 #include "DataFormats/Common/interface/Handle.h"
+#include "DataFormats/L1TParticleFlow/interface/PFCandidate.h"
 #include "FWCore/Framework/interface/stream/EDProducer.h"
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 
-#include "L1Trigger/Phase2L1ParticleFlow/interface/L1EGPuppiIsoAlgo.h"
+#include "L1Trigger/Phase2L1ParticleFlow/src/newfirmware/egamma/L1EGPuppiIsoAlgo.h"
 
 template <typename T>
 class L1TEGPuppiIsoProducer : public edm::stream::EDProducer<> {
@@ -20,10 +21,14 @@ public:
 private:
   void produce(edm::Event& iEvent, const edm::EventSetup& iSetup) override;
 
+  void prepareAlgoEgInput(const std::vector<T> &l1EGs, l1ct::EGIsoObjsEmu &egIsoObjs);
+  void prepareAlgoPuppiInput(const l1t::PFCandidateCollection &l1PFCands, l1ct::PuppiObjs &puppiObjs);
+  void addPuppiIsoToOutput(const l1ct::EGIsoObjsEmu &outEgIsoObjsEmu, std::vector<T> &outL1EGs);
+
   edm::EDGetTokenT<std::vector<T>> emToken_;
   edm::EDGetTokenT<std::vector<l1t::PFCandidate>> pfCandToken_;
 
-  std::unique_ptr<l1ct::L1EGPuppiIsoAlgo<T>> algo_;
+  std::unique_ptr<l1ct::L1EGPuppiIsoAlgo> algo_;
 
   const std::string outInstanceName_;
 };
@@ -35,7 +40,7 @@ L1TEGPuppiIsoProducer<T>::L1TEGPuppiIsoProducer(const edm::ParameterSet& iConfig
       algo_(nullptr),
       outInstanceName_(iConfig.getParameter<std::string>("outInstanceName"))
 {
-  algo_ = std::make_unique<l1ct::L1EGPuppiIsoAlgo<T>>(l1ct::L1EGPuppiIsoAlgoConfig(
+  algo_ = std::make_unique<l1ct::L1EGPuppiIsoAlgo>(l1ct::L1EGPuppiIsoAlgoConfig(
     iConfig.getParameter<std::string>("pfIsoType"),
     iConfig.getParameter<double>("pfMinPt"),
     iConfig.getParameter<double>("dRMinIso"),
@@ -49,15 +54,87 @@ template <typename T>
 void L1TEGPuppiIsoProducer<T>::produce(edm::Event& iEvent, const edm::EventSetup& iSetup) {
   auto outL1EGs = std::make_unique<std::vector<T>>();
 
+  // get the EDM collections
   edm::Handle<std::vector<T>> l1EGs;
   iEvent.getByToken(emToken_, l1EGs);
 
   edm::Handle<l1t::PFCandidateCollection> l1PFCands;
   iEvent.getByToken(pfCandToken_, l1PFCands);
 
-  algo_->run(*l1EGs, *l1PFCands, *outL1EGs);
+  // duplicate the L1 EG input collection since this module only calculates the PUPPI isolation
+  for (const auto &l1EG : *l1EGs) {
+    outL1EGs->emplace_back(l1EG);
+  }
+
+  // the collections of the isolation algo
+  l1ct::EGIsoObjsEmu egIsoObjs;
+  l1ct::EGIsoObjsEmu outEgIsoObjs;
+  l1ct::PuppiObjs puppiObjs;
+
+  // prepare the algo object collections from the EDM collections
+  prepareAlgoEgInput(*l1EGs, egIsoObjs);
+  prepareAlgoPuppiInput(*l1PFCands, puppiObjs);
+
+  // run the iso algo
+  algo_->run(egIsoObjs, puppiObjs, outEgIsoObjs);
+
+  // decode the algo output collections and make EDM collections to put in the event
+  addPuppiIsoToOutput(outEgIsoObjs, *outL1EGs);
 
   iEvent.put(std::move(outL1EGs), outInstanceName_);
+}
+
+template <typename T>
+void L1TEGPuppiIsoProducer<T>::prepareAlgoEgInput(const std::vector<T> &l1EGs, l1ct::EGIsoObjsEmu &egIsoObjs)
+{
+  for (const auto &l1EG : l1EGs) {
+    l1ct::EGIsoObjEmu obj;
+    obj.hwPt = l1ct::Scales::makePtFromFloat(l1EG.pt());
+    obj.hwEta = l1ct::Scales::makeEta(l1EG.eta());
+    obj.hwPhi = l1ct::Scales::makePhi(l1EG.phi());
+    obj.hwQual = l1EG.hwQual();
+    obj.hwIso = l1EG.hwIso();
+    egIsoObjs.emplace_back(obj);
+  }
+}
+
+template <typename T>
+void L1TEGPuppiIsoProducer<T>::prepareAlgoPuppiInput(const l1t::PFCandidateCollection &l1PFCands, l1ct::PuppiObjs &puppiObjs)
+{
+  for (const auto &l1PFCand : l1PFCands) {
+    l1ct::PuppiObj obj;
+    obj.hwPt = l1ct::Scales::makePtFromFloat(l1PFCand.pt());
+    obj.hwEta = l1ct::Scales::makeEta(l1PFCand.eta());
+    obj.hwPhi = l1ct::Scales::makePhi(l1PFCand.phi());
+    if (l1PFCand.id() == l1t::PFCandidate::Muon) {
+      obj.hwId = l1ct::ParticleID::mkMuon(l1PFCand.charge() > 0);
+    } else if (l1PFCand.id() == l1t::PFCandidate::Electron) {
+      obj.hwId = l1ct::ParticleID::mkElectron(l1PFCand.charge() > 0);
+    } else if (l1PFCand.id() == l1t::PFCandidate::ChargedHadron) {
+      obj.hwId = l1ct::ParticleID::mkChHad(l1PFCand.charge() > 0);
+    } else if (l1PFCand.id() == l1t::PFCandidate::Photon) {
+      obj.hwId = l1ct::ParticleID::PHOTON;
+    } else {
+      obj.hwId = l1ct::ParticleID::HADZERO;
+    }
+    if (obj.hwId.charged()) {
+      obj.setHwZ0(l1PFCand.hwZ0());
+      obj.setHwDxy(l1PFCand.hwDxy());
+      obj.setHwTkQuality(l1PFCand.hwTkQuality());
+    } else {
+      obj.setHwPuppiW(l1PFCand.hwPuppiWeight());
+    }
+    puppiObjs.emplace_back(obj);
+  }
+}
+
+template <typename T>
+void L1TEGPuppiIsoProducer<T>::addPuppiIsoToOutput(const l1ct::EGIsoObjsEmu &outEgIsoObjsEmu, std::vector<T> &outL1EGs)
+{
+  for (size_t i = 0; i < outEgIsoObjsEmu.size() && i < outL1EGs.size(); ++i) {
+    outL1EGs[i].setPuppiIsol(outEgIsoObjsEmu[i].floatIso(l1ct::EGIsoObjEmu::IsoType::PfIso));
+    outL1EGs[i].setPuppiIsolPV(outEgIsoObjsEmu[i].floatIso(l1ct::EGIsoObjEmu::IsoType::PfIsoPV));
+  }
 }
 
 template <typename T>
