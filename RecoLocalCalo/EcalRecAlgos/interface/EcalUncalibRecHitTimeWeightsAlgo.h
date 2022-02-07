8bc02e7b68cf534cf5e60eb129381a402e594387
Thomas Reis <thomas.reis@cern.ch>
2022-02-07 13:59:29 +0100

    Template EigenMatrixTypes to allow different dimensions for Phase 2.

--- RecoLocalCalo/EcalRecAlgos/interface/EcalUncalibRecHitTimeWeightsAlgo.h
+++ RecoLocalCalo/EcalRecAlgos/interface/EcalUncalibRecHitTimeWeightsAlgo.h
@@ -16,6 +16,7 @@
 #include "CondFormats/EcalObjects/interface/EcalPedestals.h"
 #include "CondFormats/EcalObjects/interface/EcalGainRatios.h"
 #include "CondFormats/EcalObjects/interface/EcalWeightSet.h"
+#include "DataFormats/EcalDigi/interface/EcalConstants.h"
 
 #include "RecoLocalCalo/EcalRecAlgos/interface/EigenMatrixTypes.h"
 #include "RecoLocalCalo/EcalRecAlgos/interface/EcalUncalibRecHitRecAbsAlgo.h"
@@ -26,6 +27,8 @@
 template <class C>
 class EcalUncalibRecHitTimeWeightsAlgo {
 public:
+  using FullSampleVector = typename EigenMatrixTypes<ecalPh1>::FullSampleVector;
+
   EcalUncalibRecHitTimeWeightsAlgo<C>(){};
   virtual ~EcalUncalibRecHitTimeWeightsAlgo<C>(){};
 
