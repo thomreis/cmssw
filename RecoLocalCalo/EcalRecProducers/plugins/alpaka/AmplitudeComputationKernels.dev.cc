94644800672aa66242b1ea99fedcb126deeae176
Thomas <thomas.reis@cern.ch>
2024-01-16 15:42:23 +0100

    Implement comments for AmplitudeComputationKernels.dev.cc.

--- RecoLocalCalo/EcalRecProducers/plugins/alpaka/AmplitudeComputationKernels.dev.cc
+++ RecoLocalCalo/EcalRecProducers/plugins/alpaka/AmplitudeComputationKernels.dev.cc
@@ -92,176 +92,166 @@ namespace ALPAKA_ACCELERATOR_NAMESPACE::ecal::multifit {
 
       // channel
       for (auto idx : cms::alpakatools::elements_with_stride(acc, nchannels)) {
-        if (idx < nchannels) {
-          if (static_cast<MinimizationState>(acState[idx]) == MinimizationState::Precomputed)
-            continue;
+        if (static_cast<MinimizationState>(acState[idx]) == MinimizationState::Precomputed)
+          continue;
 
-          auto const elemIdx = idx % elemsPerBlock;
+        auto const elemIdx = idx % elemsPerBlock;
 
-          // shared memory pointers
-          DataType* shrMatrixLForFnnlsStorage = shrmem + calo::multifit::MapSymM<DataType, NPULSES>::total * elemIdx;
-          DataType* shrAtAStorage =
-              shrmem + calo::multifit::MapSymM<DataType, NPULSES>::total * (elemIdx + elemsPerBlock);
+        // shared memory pointers
+        DataType* shrMatrixLForFnnlsStorage = shrmem + calo::multifit::MapSymM<DataType, NPULSES>::total * elemIdx;
+        DataType* shrAtAStorage =
+            shrmem + calo::multifit::MapSymM<DataType, NPULSES>::total * (elemIdx + elemsPerBlock);
 
-          auto* amplitudes =
-              reinterpret_cast<SampleVector*>(idx >= nchannelsEB ? uncalibRecHitsEE.outOfTimeAmplitudes()->data()
-                                                                 : uncalibRecHitsEB.outOfTimeAmplitudes()->data());
-          auto* energies = idx >= nchannelsEB ? uncalibRecHitsEE.amplitude() : uncalibRecHitsEB.amplitude();
-          auto* chi2s = idx >= nchannelsEB ? uncalibRecHitsEE.chi2() : uncalibRecHitsEB.chi2();
+        auto* amplitudes =
+            reinterpret_cast<SampleVector*>(idx >= nchannelsEB ? uncalibRecHitsEE.outOfTimeAmplitudes()->data()
+                                                               : uncalibRecHitsEB.outOfTimeAmplitudes()->data());
+        auto* energies = idx >= nchannelsEB ? uncalibRecHitsEE.amplitude() : uncalibRecHitsEB.amplitude();
+        auto* chi2s = idx >= nchannelsEB ? uncalibRecHitsEE.chi2() : uncalibRecHitsEB.chi2();
 
-          // get the hash
-          int const inputCh = idx >= nchannelsEB ? idx - nchannelsEB : idx;
-          auto const* dids = idx >= nchannelsEB ? digisDevEE.id() : digisDevEB.id();
-          auto const did = DetId{dids[inputCh]};
-          auto const isBarrel = did.subdetId() == EcalBarrel;
-          auto const hashedId = isBarrel ? ecal::reconstruction::hashedIndexEB(did.rawId())
-                                         : offsetForHashes + ecal::reconstruction::hashedIndexEE(did.rawId());
+        // get the hash
+        int const inputCh = idx >= nchannelsEB ? idx - nchannelsEB : idx;
+        auto const* dids = idx >= nchannelsEB ? digisDevEE.id() : digisDevEB.id();
+        auto const did = DetId{dids[inputCh]};
+        auto const isBarrel = did.subdetId() == EcalBarrel;
+        auto const hashedId = isBarrel ? ecal::reconstruction::hashedIndexEB(did.rawId())
+                                       : offsetForHashes + ecal::reconstruction::hashedIndexEE(did.rawId());
 
-          // inits
-          int iter = 0;
-          int npassive = 0;
+        // inits
+        int npassive = 0;
 
-          calo::multifit::ColumnVector<NPULSES, int> pulseOffsets;
+        calo::multifit::ColumnVector<NPULSES, int> pulseOffsets;
+        CMS_UNROLL_LOOP
+        for (int i = 0; i < NPULSES; ++i)
+          pulseOffsets(i) = i;
+
+        calo::multifit::ColumnVector<NPULSES, DataType> resultAmplitudes;
+        CMS_UNROLL_LOOP
+        for (int counter = 0; counter < NPULSES; ++counter)
+          resultAmplitudes(counter) = 0;
+
+        // inits
+        //SampleDecompLLT covariance_decomposition;
+        //SampleMatrix inverse_cov;
+        //        SampleVector::Scalar chi2 = 0, chi2_now = 0;
+        float chi2 = 0, chi2_now = 0;
+
+        // loop for up to max_iterations
+        for (int iter = 0; iter < max_iterations; ++iter) {
+          //inverse_cov = noisecov[idx];
+          //DataType covMatrixStorage[MapSymM<DataType, NSAMPLES>::total];
+          DataType* covMatrixStorage = shrMatrixLForFnnlsStorage;
+          calo::multifit::MapSymM<DataType, NSAMPLES> covMatrix{covMatrixStorage};
+          int counter = 0;
           CMS_UNROLL_LOOP
-          for (int i = 0; i < NPULSES; ++i)
-            pulseOffsets(i) = i;
-
-          calo::multifit::ColumnVector<NPULSES, DataType> resultAmplitudes;
-          CMS_UNROLL_LOOP
-          for (int counter = 0; counter < NPULSES; ++counter)
-            resultAmplitudes(counter) = 0;
-
-          // inits
-          //SampleDecompLLT covariance_decomposition;
-          //SampleMatrix inverse_cov;
-          //        SampleVector::Scalar chi2 = 0, chi2_now = 0;
-          float chi2 = 0, chi2_now = 0;
-
-          // loop until converge
-          while (true) {
-            if (iter >= max_iterations)
-              break;
-
-            //inverse_cov = noisecov[idx];
-            //DataType covMatrixStorage[MapSymM<DataType, NSAMPLES>::total];
-            DataType* covMatrixStorage = shrMatrixLForFnnlsStorage;
-            calo::multifit::MapSymM<DataType, NSAMPLES> covMatrix{covMatrixStorage};
-            int counter = 0;
+          for (int col = 0; col < NSAMPLES; ++col) {
             CMS_UNROLL_LOOP
-            for (int col = 0; col < NSAMPLES; ++col) {
-              CMS_UNROLL_LOOP
-              for (int row = col; row < NSAMPLES; ++row) {
-                covMatrixStorage[counter++] = noisecov[idx].coeffRef(row, col);
-              }
+            for (int row = col; row < NSAMPLES; ++row) {
+              covMatrixStorage[counter++] = noisecov[idx].coeffRef(row, col);
             }
-            update_covariance(pulse_covariance[hashedId], covMatrix, resultAmplitudes);
+          }
+          update_covariance(pulse_covariance[hashedId], covMatrix, resultAmplitudes);
 
-            // compute actual covariance decomposition
-            //covariance_decomposition.compute(inverse_cov);
-            //auto const& matrixL = covariance_decomposition.matrixL();
-            DataType matrixLStorage[calo::multifit::MapSymM<DataType, NSAMPLES>::total];
-            calo::multifit::MapSymM<DataType, NSAMPLES> matrixL{matrixLStorage};
-            calo::multifit::compute_decomposition_unrolled(matrixL, covMatrix);
+          // compute actual covariance decomposition
+          //covariance_decomposition.compute(inverse_cov);
+          //auto const& matrixL = covariance_decomposition.matrixL();
+          DataType matrixLStorage[calo::multifit::MapSymM<DataType, NSAMPLES>::total];
+          calo::multifit::MapSymM<DataType, NSAMPLES> matrixL{matrixLStorage};
+          calo::multifit::compute_decomposition_unrolled(matrixL, covMatrix);
 
-            // L * A = P
-            calo::multifit::ColMajorMatrix<NSAMPLES, NPULSES> A;
-            calo::multifit::solve_forward_subst_matrix(A, pulse_matrix[idx], matrixL);
+          // L * A = P
+          calo::multifit::ColMajorMatrix<NSAMPLES, NPULSES> A;
+          calo::multifit::solve_forward_subst_matrix(A, pulse_matrix[idx], matrixL);
 
-            // L b = s
-            float reg_b[NSAMPLES];
-            calo::multifit::solve_forward_subst_vector(reg_b, samples[idx], matrixL);
+          // L b = s
+          float reg_b[NSAMPLES];
+          calo::multifit::solve_forward_subst_vector(reg_b, samples[idx], matrixL);
 
-            // FIXME: shared mem
-            //DataType AtAStorage[MapSymM<DataType, NPULSES>::total];
-            calo::multifit::MapSymM<DataType, NPULSES> AtA{shrAtAStorage};
-            //SampleMatrix AtA;
-            SampleVector Atb;
+          // FIXME: shared mem
+          //DataType AtAStorage[MapSymM<DataType, NPULSES>::total];
+          calo::multifit::MapSymM<DataType, NPULSES> AtA{shrAtAStorage};
+          //SampleMatrix AtA;
+          SampleVector Atb;
+          CMS_UNROLL_LOOP
+          for (int icol = 0; icol < NPULSES; ++icol) {
+            float reg_ai[NSAMPLES];
+
+            // load column icol
             CMS_UNROLL_LOOP
-            for (int icol = 0; icol < NPULSES; ++icol) {
-              float reg_ai[NSAMPLES];
+            for (int counter = 0; counter < NSAMPLES; ++counter)
+              reg_ai[counter] = A(counter, icol);
 
-              // load column icol
+            // compute diagoanl
+            float sum = 0.f;
+            CMS_UNROLL_LOOP
+            for (int counter = 0; counter < NSAMPLES; ++counter)
+              sum += reg_ai[counter] * reg_ai[counter];
+
+            // store
+            AtA(icol, icol) = sum;
+
+            // go thru the other columns
+            CMS_UNROLL_LOOP
+            for (int j = icol + 1; j < NPULSES; ++j) {
+              // load column j
+              float reg_aj[NSAMPLES];
               CMS_UNROLL_LOOP
               for (int counter = 0; counter < NSAMPLES; ++counter)
-                reg_ai[counter] = A(counter, icol);
+                reg_aj[counter] = A(counter, j);
 
-              // compute diagoanl
+              // accum
               float sum = 0.f;
               CMS_UNROLL_LOOP
               for (int counter = 0; counter < NSAMPLES; ++counter)
-                sum += reg_ai[counter] * reg_ai[counter];
+                sum += reg_aj[counter] * reg_ai[counter];
 
               // store
-              AtA(icol, icol) = sum;
-
-              // go thru the other columns
-              CMS_UNROLL_LOOP
-              for (int j = icol + 1; j < NPULSES; ++j) {
-                // load column j
-                float reg_aj[NSAMPLES];
-                CMS_UNROLL_LOOP
-                for (int counter = 0; counter < NSAMPLES; ++counter)
-                  reg_aj[counter] = A(counter, j);
-
-                // accum
-                float sum = 0.f;
-                CMS_UNROLL_LOOP
-                for (int counter = 0; counter < NSAMPLES; ++counter)
-                  sum += reg_aj[counter] * reg_ai[counter];
-
-                // store
-                //AtA(icol, j) = sum;
-                AtA(j, icol) = sum;
-              }
-
-              // Atb accum
-              float sum_atb = 0.f;
-              CMS_UNROLL_LOOP
-              for (int counter = 0; counter < NSAMPLES; ++counter)
-                sum_atb += reg_ai[counter] * reg_b[counter];
-
-              // store atb
-              Atb(icol) = sum_atb;
+              //AtA(icol, j) = sum;
+              AtA(j, icol) = sum;
             }
 
-            // FIXME: shared mem
-            //DataType matrixLForFnnlsStorage[MapSymM<DataType, NPULSES>::total];
-            calo::multifit::MapSymM<DataType, NPULSES> matrixLForFnnls{shrMatrixLForFnnlsStorage};
+            // Atb accum
+            float sum_atb = 0.f;
+            CMS_UNROLL_LOOP
+            for (int counter = 0; counter < NSAMPLES; ++counter)
+              sum_atb += reg_ai[counter] * reg_b[counter];
 
-            calo::multifit::fnnls(AtA,
-                                  Atb,
-                                  //amplitudes[idx],
-                                  resultAmplitudes,
-                                  npassive,
-                                  pulseOffsets,
-                                  matrixLForFnnls,
-                                  1e-11,
-                                  500,
-                                  16,
-                                  2);
-
-            calo::multifit::calculateChiSq(matrixL, pulse_matrix[idx], resultAmplitudes, samples[idx], chi2_now);
-
-            auto const deltachi2 = chi2_now - chi2;
-            chi2 = chi2_now;
-
-            if (std::abs(deltachi2) < 1e-3)
-              break;
-
-            //---- AM: TEST
-            //---- it was 3 lines above, now here as in the CPU version
-            ++iter;
+            // store atb
+            Atb(icol) = sum_atb;
           }
 
-          // store to global output values
-          // FIXME: amplitudes are used in global directly
-          chi2s[inputCh] = chi2;
-          energies[inputCh] = resultAmplitudes(5);
+          // FIXME: shared mem
+          //DataType matrixLForFnnlsStorage[MapSymM<DataType, NPULSES>::total];
+          calo::multifit::MapSymM<DataType, NPULSES> matrixLForFnnls{shrMatrixLForFnnlsStorage};
 
-          CMS_UNROLL_LOOP
-          for (int counter = 0; counter < NPULSES; ++counter)
-            amplitudes[inputCh](counter) = resultAmplitudes(counter);
+          calo::multifit::fnnls(AtA,
+                                Atb,
+                                //amplitudes[idx],
+                                resultAmplitudes,
+                                npassive,
+                                pulseOffsets,
+                                matrixLForFnnls,
+                                1e-11,
+                                500,
+                                16,
+                                2);
+
+          calo::multifit::calculateChiSq(matrixL, pulse_matrix[idx], resultAmplitudes, samples[idx], chi2_now);
+
+          auto const deltachi2 = chi2_now - chi2;
+          chi2 = chi2_now;
+
+          if (std::abs(deltachi2) < 1e-3)
+            break;
         }
+
+        // store to global output values
+        // FIXME: amplitudes are used in global directly
+        chi2s[inputCh] = chi2;
+        energies[inputCh] = resultAmplitudes(5);
+
+        CMS_UNROLL_LOOP
+        for (int counter = 0; counter < NPULSES; ++counter)
+          amplitudes[inputCh](counter) = resultAmplitudes(counter);
       }
     }
   };
@@ -297,7 +287,7 @@ namespace ALPAKA_ACCELERATOR_NAMESPACE::ecal::multifit {
                         scratch.hasSwitchToGain1DevBuf.data(),
                         scratch.isSaturatedDevBuf.data(),
                         scratch.acStateDevBuf.data(),
-                        50);
+                        50);  // maximum number of fit iterations
   }
 
 }  // namespace ALPAKA_ACCELERATOR_NAMESPACE::ecal::multifit
